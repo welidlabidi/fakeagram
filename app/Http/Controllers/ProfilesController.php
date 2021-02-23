c7f1087c62d935ddadf499df31ef69a702df89ec
welidlabidi <56021644+welidlabidi@users.noreply.github.com>
2021-02-23 17:16:57 +0100

    fakeagram

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -8,11 +8,13 @@ use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    public function index($user)
+    public function index(User $user)
     {
-        $user = User::findOrFail($user);
-        return view('profiles.index', [
-            'user' => $user,
-        ]);
+        return view('profiles.index', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('profiles.edit', compact('user'));
     }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,21 +10,28 @@
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1> {{ $user->username }} </h1>
                 <a href="/p/create">add new post</a>
+
             </div>
+
             <div class="d-flex">
-                <div class="pr-5"><strong>134</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>12k</strong> followers</div>
                 <div class="pr-5"><strong>232</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
             <div>{{$user->profile->description}} </div>
-            <div><a href="">{{$user->profile->url}}</a></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <a href="">{{$user->profile->url}}</a>
+                <a href="/profile/{{ $user->id }}/edit">Edit profile</a>
+            </div>
         </div>
     </div>
-    <div class="row pt-5">
+    <div class="row pt-5 pb-4">
         @foreach($user->posts as $post)
         <div class="col-4">
-            <img src="/storage/{{ $post->image }}" class="w-100" alt="" />
+            <a href="/p/{{$post->id}}">
+                <img src="/storage/{{ $post->image }}" class="w-100" alt="" />
+            </a>
             <h5>{{ $post->caption  }}</h5>
         </div>
 
