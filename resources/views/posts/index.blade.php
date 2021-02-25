748890f4628c016149bc5741b2bcfeff38589e35
welidlabidi <56021644+welidlabidi@users.noreply.github.com>
2021-02-25 13:55:55 +0100

    fakeagram

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,37 +4,23 @@
 <div class="container">
     @foreach($posts as $post)
     <div class="row">
-        <div class="col-8">
-            <img src="/storage/{{ $post->image }}" class="w-100" alt="" />
-        </div>
-        <div class="col-4">
-            <div>
-                <div class="d-flex align-items-center">
-                    <div class="pr-3">
-                        <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" alt=""
-                            style="max-width:40px;" />
-                    </div>
-                    <div>
-                        <div>
-                            <a href="/profile/{{ $post->user->id }}">
-                                <span class="text-dark">{{$post->user->username}}</span>
-                            </a>
-                            <a href="" class="pl-4">Follow</a>
-                        </div>
-                    </div>
-                </div>
-                <hr>
-                <p>
-                    <span>
-                        <a href="/profile/{{ $post->user->id }}">
-                            <span class="text-dark">{{$post->user->username}}</span>
-                        </a>
-                    </span>
-                    {{ $post->caption}}
-                </p>
-            </div>
+        <div class="col-4 offset-4">
+            <a href="/profile/{{$post->user->id}}"><img src="/storage/{{ $post->image }}" class="w-100" alt="" /></a>
         </div>
     </div>
-    @endforeach
+    <div class="row pt-2 pb-4">
+        <div class="col-4 offset-4">
+            <p>
+                <span class="font-weight-bold">
+                    <a href="/profile/{{ $post->user->id }}">
+                        <span class="text-dark">{{$post->user->username}}</span>
+                    </a>
+                </span>
+                {{ $post->caption}}
+            </p>
+        </div>
+    </div>
+</div>
+@endforeach
 </div>
 @endsection
